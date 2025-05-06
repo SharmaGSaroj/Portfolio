65ebad679c3eccbab5e87fae2deb547e1ed60368
SharmaGSaroj <91160959+SharmaGSaroj@users.noreply.github.com>
2025-05-06 12:07:01 -0400

    fixed next to react

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 // Register service worker
 if ('serviceWorker' in navigator) {
